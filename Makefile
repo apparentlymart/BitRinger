c1ed3fc98863b1c38d966169a86eb0267e1b9c28
Martin Atkins <mart@degeneration.co.uk>
2012-12-09 19:16:04 -0800

    Add an initial example of the SDL backend.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 
-all:
+all: examples
+
+examples: examples/sdl_smile
 
 test: tests/test
 	@tests/test
@@ -7,5 +9,8 @@ test: tests/test
 tests/test: tests/test.cpp src/bitcanvas.cpp include/bitcanvas.h
 	@g++ -Iinclude tests/test.cpp src/bitcanvas.cpp -o tests/test
 
-.PHONY: all test
+examples/sdl_smile: examples/sdl_smile.cpp src/bitcanvas.cpp src/bitcanvas_sdl.cpp include/bitcanvas.h include/bitcanvas.h
+	g++ -Iinclude examples/sdl_smile.cpp src/bitcanvas.cpp src/bitcanvas_sdl.cpp -lSDL -o examples/sdl_smile
+
+.PHONY: all test examples
 
---
+++ examples/sdl_smile.cpp
@@ -0,0 +1,28 @@
+
+/***************************************
+
+ Simple example of the raw pixel writing
+ API against the SDL backend.
+
+****************************************/
+
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <bitcanvas_sdl.h>
+
+int main(int argc, char **argv) {
+
+    BitCanvas_SDL c(8, 8);
+
+    c.write_8_pixels(0, 0, 0b01111110);
+    c.write_8_pixels(0, 1, 0b10000001);
+    c.write_8_pixels(0, 2, 0b10100101);
+    c.write_8_pixels(0, 3, 0b10000001);
+    c.write_8_pixels(0, 4, 0b10100101);
+    c.write_8_pixels(0, 5, 0b10011001);
+    c.write_8_pixels(0, 6, 0b10000001);
+    c.write_8_pixels(0, 7, 0b01111110);
+    c.update_all();
+
+    c.wait_until_window_closed();
+}
